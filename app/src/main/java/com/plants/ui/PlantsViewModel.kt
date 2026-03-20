c2e0cb920c4e3bdaba9ab291bbe457aecd6b577c
yuta <yf1229@proton.me>
2026-03-20 19:04:16 +0900

    Refactor PlantsViewModel to simplify updateNodeId method name

--- app/src/main/java/com/plants/ui/PlantsViewModel.kt
+++ app/src/main/java/com/plants/ui/PlantsViewModel.kt
@@ -1,21 +1,7 @@
 package com.plants.ui
 
-import androidx.camera.core.ExperimentalGetImage
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import com.google.mlkit.vision.barcode.BarcodeScanning
-import com.google.mlkit.vision.common.InputImage
 import com.plants.data.PlantsRepository
 
 class PlantsViewModel(
@@ -23,7 +9,7 @@ class PlantsViewModel(
     private val plantsRepository: PlantsRepository
 ) : ViewModel() {
     private val plantIdKey = "plantId"
-    fun updateNodeId(id: Int) {
+    fun updateId(id: Int) {
         savedStateHandle[plantIdKey] = id
     }
 }
