8fd6f5a65291eb1d2ac0c556e237ee52ccfcced4
dev <dev@dev.com>
2026-03-25 15:53:59 +0900

    Thanks to GODs. I'll never give up.

--- app/src/main/java/com/plants/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/plants/ui/AppViewModelProvider.kt
@@ -1,17 +1,23 @@
 package com.plants.ui
 
+import android.app.Application
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.plants.MainActivity
+import com.plants.PlantsApplication
+import com.plants.data.AppDataContainer
 
 object AppViewModelProvider {
     val Factory = viewModelFactory {
-            initializer {
-                PlantsViewModel(
-                    savedStateHandle = this.createSavedStateHandle(),
-                    plantsRepository = MainActivity().container.plantsRepository
-                )
-            }
+        initializer {
+            val app = checkNotNull(this[APPLICATION_KEY]) as Application
+            val container = (app as? PlantsApplication)?.container ?: AppDataContainer(app)
+
+            PlantsViewModel(
+                savedStateHandle = createSavedStateHandle(),
+                plantsRepository = container.plantsRepository
+            )
+        }
     }
 }
